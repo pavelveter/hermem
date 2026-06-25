657040490b804ca993c266f761f7c3ed770b2e15
Borisov Pavel <kayjaylitee@gmail.com>
2026-06-25 14:57:58 +0300

    refactor(retrieval): thread Tracer through RetrieveContext opts

--- src/internal/tracing/context.go
+++ src/internal/tracing/context.go
@@ -5,6 +5,18 @@ import (
 )
 
 type reqIDKey struct{}
+type tracerKey struct{}
+
+func WithTracer(ctx context.Context, tracer Tracer) context.Context {
+	return context.WithValue(ctx, tracerKey{}, tracer)
+}
+
+func TracerFrom(ctx context.Context) Tracer {
+	if t, ok := ctx.Value(tracerKey{}).(Tracer); ok {
+		return t
+	}
+	return NoopTracer{}
+}
 
 func WithSpan(ctx context.Context, span Span) context.Context {
 	return context.WithValue(ctx, spanKey{}, span)
